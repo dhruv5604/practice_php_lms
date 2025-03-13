3c9561e7a4a2349ad04db66a125ef9fbc55a6461
dhruv5604 <dhruvsolanki5604@gmail.com>
2025-03-13 12:21:41 +0530

    variables scope

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $x = 5;
+        function my(){
+            global $x;
+            echo "inside function ".$x . "</br>";
+        }
+        my();
+        echo "outside function ".$x . "</br>";
+
+        function my2(){
+            static $x = 0;
+            echo $x . "<br>";
+            $x++;
+        }
+
+        my2();
+        my2();
+        my2();
+    ?>
+</body>
+</html>
